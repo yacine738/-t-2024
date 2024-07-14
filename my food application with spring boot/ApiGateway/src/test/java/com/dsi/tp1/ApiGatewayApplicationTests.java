aa714078575770fcf49590d5330705a2dc83f13b
mohamed yassine souissi <114877560+yacine738@users.noreply.github.com>
2024-07-14 13:43:03 +0100

    Add files via upload

---
+++ my food application with spring boot/ApiGateway/src/test/java/com/dsi/tp1/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsi.tp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
